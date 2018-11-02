e2d45012d030ac6d1f6df254e859f2211727b3d1
Sreetam Das <sreetamdas@gmail.com>
2018-11-02 16:40:00 +0530

    wlorking

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -6,29 +6,60 @@ export default class Tables extends React.Component {
 		super(props);
 
 		this.renderTable = this.renderTable.bind(this);
-		this.state = { active: true };
+		this.state = {
+			active: true,
+			data: {
+				ip: ["10.56.0.27", "10.56.1.19", "10.56.0.26", "10.56.1.10"],
+				name: [
+					"app-orange-53dc4be6f62dd408be35406f2486247c-2rfqx",
+					"app-orange-53dc4be6f62dd408be35406f2486247c-b7whc",
+					"app-orange-53dc4be6f62dd408be35406f2486247c-z5r48",
+					"database-cfwp8"
+				]
+			}
+		};
 	}
 
 	componentDidMount = () => {
-		this.fetchData();
+		// this.fetchData();
 	};
 	renderTable = () => {
-		// const data = [ ...this.state.data ];
+		const data = { ...this.state.data };
+		console.log({ data });
+
+		const ips = [...data[`ip`]];
+		const names = [...data[`name`]];
+
+		console.log({ ips });
+		console.log({ names });
 
 		return (
-			<tr>
-				<td>naame</td>
-				<td>iip</td>
-				<td>statuss</td>
-				<td>
-					<button className="btn btn-md btn-danger">
-						Kill Instance
-					</button>
-				</td>
-			</tr>
+			<React.Fragment>
+				{ips.map((ip, index) => (
+					<tr>
+						<td>{names[index]}</td>
+						<td>{ip}</td>
+						<td>statuss</td>
+						<td>
+							<button
+								className="btn btn-md btn-danger"
+								onClick={e =>
+									this.killNode(e, `${names[index]}`)
+								}
+							>
+								Kill Instance
+							</button>
+						</td>
+					</tr>
+				))}
+			</React.Fragment>
 		);
 	};
 
+	killNode = (e, node) => {
+		e.preventDefault();
+		console.log(node);
+	};
 	fetchData = () => {
 		axios
 			.get("http://35.237.161.200:5000/dashboard/")
