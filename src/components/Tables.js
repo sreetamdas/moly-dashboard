3d9b5e6148c8424ae4458dd4717629460e72b0db
Sreetam Das <sreetamdas@gmail.com>
2018-11-02 16:00:31 +0530

    plis no vim

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -5,12 +5,33 @@ export default class Tables extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.renderTable = this.renderTable.bind(this);
+		this.state = { active: true };
 	}
+
+	componentDidMount = () => {
+		this.fetchData();
+	};
+	renderTable = () => {
+		// const data = [ ...this.state.data ];
+
+		return (
+			<tr>
+				<td>naame</td>
+				<td>iip</td>
+				<td>statuss</td>
+				<td>
+					<button className="btn btn-md btn-danger">
+						Kill Instance
+					</button>
+				</td>
+			</tr>
+		);
+	};
 
 	fetchData = () => {
 		axios
-			.get()
+			.get("http://35.237.161.200:5000/dashboard/")
 			.then(response => {
 				console.log(response);
 			})
@@ -22,7 +43,18 @@ export default class Tables extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<table className="table" />
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>IP</th>
+							<th>Status</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+
+					<tbody>{this.state.active && this.renderTable()}</tbody>
+				</table>
 			</React.Fragment>
 		);
 	}
