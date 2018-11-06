62c3d048ad59fb300f517d6f6ac5cb9bf7f3653a
Sreetam Das <sreetamdas@gmail.com>
2018-11-06 06:09:32 +0000

    build

---
+++ build/precache-manifest.8c123b919cca3ac76f81e46e4e628849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f64a7e99e22141837829",
+    "url": "/static/js/main.f64a7e99.chunk.js"
+  },
+  {
+    "revision": "d328a8f9e4d990673a46",
+    "url": "/static/js/1.d328a8f9.chunk.js"
+  },
+  {
+    "revision": "f64a7e99e22141837829",
+    "url": "/static/css/main.c9c152f5.chunk.css"
+  },
+  {
+    "revision": "d328a8f9e4d990673a46",
+    "url": "/static/css/1.89046612.chunk.css"
+  },
+  {
+    "revision": "488cc9af06cd5933a141c31b158bef15",
+    "url": "/index.html"
+  }
+];
